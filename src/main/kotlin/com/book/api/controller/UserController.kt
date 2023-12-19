d5bc7def182a61cf4ed9cfdcfd765eaafcc4e20f
Daniil Popov <popov@example.com>
2023-12-20 00:09:21 +0300

    authentication demo

--- src/main/kotlin/com/book/api/controller/UserController.kt
+++ src/main/kotlin/com/book/api/controller/UserController.kt
@@ -2,54 +2,30 @@ package com.book.api.controller
 
 import com.book.api.entity.Book
 import com.book.api.entity.User
-import com.book.api.service.SecurityService
-import org.springframework.ui.Model;
 import com.book.api.service.UserService
-import com.book.api.validator.UserValidator
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
-
 
 @Controller
 @RequestMapping("/user")
-class UserController (private val userService: UserService, private val securityService: SecurityService,
-    private val userValidator: UserValidator) {
-
-//    @GetMapping("/get/token/{idToken}")
-//    @ResponseBody
-//    fun checkIdTokenUser(@PathVariable idToken: String) = userService.checkIdTokenUser(idToken)
-
-//    @GetMapping("/get/idToken/{idToken}")
-//    @ResponseBody
-//    fun findUserByIdToken(@PathVariable idToken: String) = userService.findUserByIdToken(idToken)
-
-    @GetMapping("/registration")
-    fun registration(model: Model): String {
-        if (securityService.isAuthenticated) {
-            return "redirect:/"
-        }
-        model.addAttribute("userForm", User())
-        return "registration"
-    }
+class UserController (private val userService: UserService) {
 
     @PostMapping("/registration")
     @ResponseBody
-    fun registration(@RequestBody user: User, bindingResult: BindingResult?, model: Model?): String {
-        userValidator.validate(user, bindingResult)
+    fun registration(@RequestBody user: User): ResponseEntity<User> {
+        if(userService.findByEmail(user.email)!=null) return ResponseEntity.status(HttpStatus.CONFLICT).body(user)
         userService.save(user)
-        securityService.autoLogin(user.name, user.password)
-        return "redirect:/"
+        var temp_user = userService.findByEmail(user.email)!!
+        return ResponseEntity.ok(user)
     }
 
-    @GetMapping("/login")
-    fun login(model: Model, error: String?, logout: String?): String {
-        if (securityService.isAuthenticated()) {
-            return "redirect:/"
-        }
-        if (error != null) model.addAttribute("error", "Ваш логин и/или пароль не верны.")
-        if (logout != null) model.addAttribute("message", "Вы успешно вышли из аккаунта.")
-        return "login"
+    @PostMapping("/login")
+    @ResponseBody
+    fun login(@RequestBody user: User): ResponseEntity<User> {
+        if(userService.findByEmail(user.email)!=null && userService.auth(user)) return ResponseEntity.ok(user)
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(user)
     }
 
     @PostMapping("/new")
--- src/main/kotlin/com/book/api/repository/UserRepository.kt
+++ src/main/kotlin/com/book/api/repository/UserRepository.kt
@@ -3,11 +3,10 @@ package com.book.api.repository
 import com.book.api.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
-//    fun findByIdToken(idToken: String): User?
-fun findByUsername(username: String?): User?
-    fun findById(Id: Long?): Optional<User?>?
+    fun findByName(name: String?): User?
+
+    fun findByEmail(email: String?): User?
 }
