a34ec9b59256c43c3b9399a3878e2aad778d5b59
Daniil Popov <popov@example.com>
2023-12-21 20:47:31 +0300

    get reviews by bookId

--- src/main/kotlin/com/book/api/controller/ReviewController.kt
+++ src/main/kotlin/com/book/api/controller/ReviewController.kt
@@ -10,13 +10,19 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/review")
 class ReviewController(private val reviewService: ReviewService) {
 
-    @GetMapping("/add")
+    @PostMapping("/add")
     @ResponseBody
     fun addReview(@RequestBody review: Review): Review {
         return reviewService.add(review)
     }
 
-    @DeleteMapping("/{id}")
+    @PostMapping("/get/{id}")
+    @ResponseBody
+    fun addReview(@PathVariable id: Long): List<Review> {
+        return reviewService.getAllByIdBook(id)
+    }
+
+    @PostMapping("/{id}")
     fun deleteReview(@PathVariable id: Long): ResponseEntity<String> {
         reviewService.delete(id)
         return ResponseEntity.ok("Review with id: $id has been deleted")
