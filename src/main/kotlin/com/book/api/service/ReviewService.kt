395f39d91c0e47635eacdd6e760e9ebef84b8cc8
Valyon1886 <64031088+Valyon1886@users.noreply.github.com>
2023-12-21 20:47:31 +0300

    get reviews by bookId

--- src/main/kotlin/com/book/api/service/ReviewService.kt
+++ src/main/kotlin/com/book/api/service/ReviewService.kt
@@ -1,20 +1,25 @@
 package com.book.api.service
 
 import com.book.api.entity.Review
+import com.book.api.repository.BookRepository
 import com.book.api.repository.ReviewRepository
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 
 @Service
 //@Component
-class ReviewService(private val reviewRepository: ReviewRepository) {
+class ReviewService(private val reviewRepository: ReviewRepository, private val bookRepository: BookRepository) {
     fun add(review: Review): Review {
         return reviewRepository.save(review)
     }
 
+    fun getAllByIdBook(id: Long): List<Review> {
+        return bookRepository.findById(id).get().reviews!!
+    }
+
     fun delete(id: Long): String {
         reviewRepository.deleteById(id)
-        return "OK"//Response.isOk();  //  TODO Сделать через json
+        return "OK"//Response.isOk(); // TODO Сделать через json
     }
 
 }
