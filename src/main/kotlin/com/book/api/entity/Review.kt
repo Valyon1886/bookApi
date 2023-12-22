f5e6b329cf45dc753ac4e29f8d25ca43c4eb281f
Daniil Popov <popov@example.com>
2023-12-22 19:09:59 +0300

    Review change

--- src/main/kotlin/com/book/api/entity/Review.kt
+++ src/main/kotlin/com/book/api/entity/Review.kt
@@ -10,8 +10,9 @@ data class Review(
     @Column(nullable = false) var rating: Int,
     @Column(nullable = false) val reviewDate: String,
     @Column(nullable = false) val userId: Long,
+    @Column(nullable = false) val userName: String,
     @Id @GeneratedValue val id: Long? = null
 )
 {
-    constructor() : this("test", 1, "123", 1)
+    constructor() : this("test", 1, "123", 1, "")
 }
